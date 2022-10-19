83a34478a8dd9b333612d81259a97befb39eac92
Nicola De Sanctis <nicoladesanctis99@gmail.com>
2022-10-19 10:36:19 +0200

    INIT: define initial setup code

---
+++ client/src/components/UI/Layout.tsx
@@ -0,0 +1,19 @@
+import { FC } from 'react'
+import Header from './Header'
+import Footer from './Footer'
+
+interface Props {
+  children: React.ReactNode
+}
+
+const Layout: FC<Props> = ({ children }) => {
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </>
+  )
+}
+
+export default Layout
