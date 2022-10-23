c9c08c167aed9a377afeab03b9ccb5750ed0cda4
Nicola De Sanctis <nicoladesanctis99@gmail.com>
2022-10-23 17:09:54 +0200

    test: add testing for postsreducer

---
+++ client/src/utils/mock/mockupPosts.ts
@@ -0,0 +1,50 @@
+export const postsMockup = [
+  {
+    userId: 1,
+    _id: 1,
+    title: 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
+    body: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto',
+  },
+  {
+    userId: 1,
+    _id: 2,
+    title: 'qui est esse',
+    body: 'est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla',
+  },
+]
+
+export const newPost = {
+  userId: 1,
+  _id: 3,
+  title: 'ea molestias quasi exercitationem repellat qui ipsa sit aut',
+  body: 'et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut',
+}
+
+export const postToRemove = {
+  userId: 1,
+  _id: 2,
+  title: 'qui est esse',
+  body: 'est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla',
+}
+
+export const updatedPost = {
+  userId: 1,
+  _id: 2,
+  title: 'updated',
+  body: 'et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut',
+}
+
+export const updatedPosts = [
+  {
+    userId: 1,
+    _id: 1,
+    title: 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
+    body: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto',
+  },
+  {
+    userId: 1,
+    _id: 2,
+    title: 'updated',
+    body: 'et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut',
+  },
+]
--- client/src/utils/postsReducer.ts
+++ client/src/utils/postsReducer.ts
@@ -4,16 +4,29 @@ import post from '../typescript/interface/post'
 function postsReducer(state: post[], action: actionPostsReducer) {
   if (action.type === 'UPDATE' && !Array.isArray(action.payload)) {
     const newState = state.map((post) => {
-      if (!Array.isArray(action.payload) && post._id === action.payload._id) return action.payload
+      if (!Array.isArray(action.payload) && post._id === action.payload?._id) return action.payload
       return post
     })
     return newState
   } else if (action.type === 'REMOVE' && !Array.isArray(action.payload)) {
     const newState = state.filter((post) => {
-      if (!Array.isArray(action.payload) && post._id !== action.payload._id) return true
+      if (!Array.isArray(action.payload) && post._id !== action.payload?._id) return true
     })
     return newState
-  } else if (action.type === 'ADD' && !Array.isArray(action.payload)) {
+  } else if (
+    action.type === 'ADD' &&
+    !Array.isArray(action.payload) &&
+    typeof action.payload === 'object' &&
+    action.payload !== null
+  ) {
+    const id = action.payload._id
+    let alreadyInPosts = false
+    state.forEach((post) => {
+      if (post._id === id) {
+        alreadyInPosts = true
+      }
+    })
+    if (alreadyInPosts) return state
     const newState: post[] = [...state, action.payload]
     return newState
   } else if (action.type === 'GET' && Array.isArray(action.payload)) {
